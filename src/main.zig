e371c525400e62dd29cf1b52a13f92c97f3c73c1
Inasa <me@inasa.dev>
2025-07-14 17:05:08 +0800

    feat: add ANYZIG_DATA_DIR env var support

--- src/main.zig
+++ src/main.zig
@@ -52,10 +52,14 @@ const global = struct {
 
     fn getAppDataDir() ![]const u8 {
         if (cached_app_data_dir == null) {
-            cached_app_data_dir = if (std.fs.getAppDataDir(arena, "anyzig")) |dir|
-                .{ .ok = dir }
-            else |e|
-                .{ .err = e };
+            const app_data_dir = blk: {
+                if (std.process.getEnvVarOwned(arena, "ANYZIG_DATA_DIR")) |custom_dir| {
+                    break :blk custom_dir;
+                } else |_| {
+                    break :blk try std.fs.getAppDataDir(arena, "anyzig");
+                }
+            };
+            cached_app_data_dir = .{ .ok = app_data_dir };
         }
         return switch (cached_app_data_dir.?) {
             .ok => |d| d,
@@ -373,7 +377,7 @@ pub fn main() !void {
         break :blk .{ .{ .semantic = try determineSemanticVersion(arena, build_root) }, false };
     };
 
-    const app_data_path = try std.fs.getAppDataDir(arena, "anyzig");
+    const app_data_path = try global.getAppDataDir();
     defer arena.free(app_data_path);
     log.info("appdata '{s}'", .{app_data_path});
 
